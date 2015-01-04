60b30629b291e3c319d5ae8e9181a7c28aaba037
Stephan Conrad <stephan.conrad@gmail.com>
2015-01-04 17:45:56 +0100

    Role Management

--- clubCore.php
+++ clubCore.php
@@ -11,19 +11,133 @@
  * Domain Path: /locale/
  * License: GPL2
  */
-
 /*  Copyright 2015  Stephan Conrad  (email : stephan.conrad@gmail.com)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+defined('ABSPATH') or die("No script kiddies please!");
+
+class ClubCore {
+
+    protected $PLUGIN_DIR;
+    protected $INSTALL_FILE;
+    protected static $TEXT_DOMAIN = "clubCore";
+    protected static $INSTANCE = NULL;
+    protected $_menuSlug = "clubCore_menu";
+    protected $vars = array();
+
+    public static function getInstance() {
+        if (self::$INSTANCE == NULL) {
+            self::$INSTANCE = new ClubCore();
+        }
+        return self::$INSTANCE;
+    }
+
+    private function __construct() {
+        $this->PLUGIN_DIR = dirname(__FILE__);
+        $this->INSTALL_FILE = $this->PLUGIN_DIR . DIRECTORY_SEPARATOR . "install.php";
+
+        $this->init();
+    }
+
+    public function init() {
+        global $wpdb;
+        load_plugin_textdomain(self::$TEXT_DOMAIN, false, basename(dirname(__FILE__)) . '/locale');
+        $this->table_name_roles = $wpdb->prefix . "clubCore_roles";
+        $this->table_name_caps = $wpdb->prefix . "clubCore_caps";
+        $this->table_name_caps_roles = $wpdb->prefix . "clubCore_caps_roles";
+        register_activation_hook(__FILE__, array(&$this, 'clubCore_install'));
+        add_action('admin_menu', array(&$this, "add_menu"));
+    }
+
+    public function add_menu() {
+        add_menu_page(
+                __("Club", self::$TEXT_DOMAIN), __("Club", self::$TEXT_DOMAIN), 'edit_others_posts', $this->menuSlug, array(&$this, "menu_index")
+        );
+    }
+
+    public function menu_index() {
+        echo "Test";
+    }
+
+    public function addRole($name, $displayName, $parrent) {
+        global $wpdb;
+        $parrentRole = get_role($parrent);
+        $role = add_role($name, $displayName, $parrentRole->capabilities);
+        if ($role == null) {
+            $role = get_role($name);
+        } else {
+            $wpdb->insert(
+                    $this->table_name_roles, array(
+                        "name" => $name,
+                        "displayName" => $displayName,
+                        "parrent" => $parrent
+                    )
+            );
+            $rid = $wpdb->insert_id;
+        }
+    }
+
+    public function __get($name) {
+        if ($name == "menuSlug") {
+            return $this->_menuSlug;
+        } else {
+            return $this->vars[$name];
+        }
+    }
+
+    public function __set($name, $value) {
+        $this->vars[$name] = $value;
+    }
+
+    public function clubCore_install() {
+        global $wpdb;
+
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $sql = array();
+        $sql[] = "CREATE TABLE $this->table_name_roles (
+            rid int NOT NULL AUTO_INCREMENT,
+            name varchar(100) NOT NULL,
+            displayName varchar(100) NOT NULL,
+            parrent varchar(100) NOT NULL,
+            PRIMARY KEY  (rid)
+          ) $charset_collate;";
+
+        $sql[] = "CREATE TABLE $this->table_name_caps (
+            cid int NOT NULL AUTO_INCREMENT,
+            name varchar(100) NOT NULL,
+            PRIMARY KEY  (cid)
+          ) $charset_collate;";
+
+        $sql[] = "CREATE TABLE $this->table_name_caps_roles (
+            rid INT NOT NULL,
+            cid INT NOT NULL,
+            INDEX role_index (rid),
+            INDEX cap_index (cid),
+            FOREIGN KEY (rid) REFERENCES $this->table_name_roles(rid),
+            FOREIGN KEY (cid) REFERENCES $this->table_name_caps(cid),
+            PRIMARY KEY  (rid, cid)
+          ) $charset_collate;";
+
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta($sql);
+
+        //$this->addRole("clubAdmin", "Club Admin", "administrator");
+    }
+
+}
+
+ClubCore::getInstance();
